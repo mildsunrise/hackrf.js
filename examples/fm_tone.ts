77d053e7518c95558447643cda2b7bc66b506a5f
Alba Mendez <me@alba.sh>
2020-08-27 11:22:19 +0200

    📝 simplify example, round values

--- examples/fm_tone.ts
+++ examples/fm_tone.ts
@@ -3,7 +3,6 @@
  */
 
 import { open } from '..'
-const TAU = 2 * Math.PI
 
 async function main() {
 	const fs = 2e6
@@ -19,41 +18,42 @@ async function main() {
 	await device.setTxVgaGain(30)
 	await device.setSampleRate(fs)
 
-	console.log(`Transmitting at ${(carrierFrequency/1e6).toFixed(2)}MHz...`)
 	const tone = makeToneGenerator(fs, toneFrequency, toneAmplitude)
-	const modulator = makeFreqModulator(fs, carrierDeviation, carrierAmplitude * 127)
+	const modulator = makeFreqModulator(fs, carrierDeviation, carrierAmplitude)
+	const signal = () => quantize( modulator(tone()) )
+
+	console.log(`Transmitting at ${(carrierFrequency/1e6).toFixed(2)}MHz...`)
 	process.on('SIGINT', () => device.requestStop())
 	await device.transmit(buffer => {
 		const samples = buffer.length / 2
 		for (let n = 0; n < samples; n++)
-			[ buffer[n * 2 + 0], buffer[n * 2 + 1] ] = modulator(tone())
+			buffer.set(signal(), n * 2)
 	})
 	console.log('\nDone, exiting')
 }
 main()
 
 type Complex = [number, number]
+const TAU = 2 * Math.PI
 
-function makeBlock<O, I=void>(func: () => Iterator<O, void, I>): (x: I) => O {
-	const gen = func()
-	return (x) => (gen.next(x).value as O)
-}
-
-const makeToneGenerator = (fs: number, frequency: number, amplitude: number = 1) => makeBlock<number>(function*() {
+function makeToneGenerator(fs: number, frequency: number, amplitude: number = 1) {
 	let phase = 0
 	const delta = (TAU * Math.abs(frequency) / fs) % TAU
-	while (true) {
-		yield amplitude * Math.cos(phase)
+	return () => {
 		phase += delta
 		phase > TAU && (phase -= TAU)
+		return amplitude * Math.cos(phase)
 	}
-})
+}
 
-const makeFreqModulator = (fs: number, maxDeviation: number, amplitude: number = 1) => makeBlock<Complex, number>(function*() {
+function makeFreqModulator(fs: number, maxDeviation: number, amplitude: number = 1) {
 	let phase = 0
 	const delta = TAU * maxDeviation / fs
-	while (true) {
-		const next = yield [ amplitude * Math.cos(phase), amplitude * Math.sin(phase) ]
-		phase = (phase + delta * next) % TAU
+	return (x: number): Complex => {
+		phase = (phase + delta * x) % TAU
+		return [ amplitude * Math.cos(phase), amplitude * Math.sin(phase) ]
 	}
-})
+}
+
+const quantize = ([i, q]: Complex): Complex =>
+	[ Math.round(i * 127), Math.round(q * 127) ]
