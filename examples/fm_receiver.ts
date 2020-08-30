53ad349312c2cc0a7a27bea89a713212083f8366
Alba Mendez <me@alba.sh>
2020-08-30 10:38:31 +0200

    📝 use correct terminology in examples

--- examples/fm_receiver.ts
+++ examples/fm_receiver.ts
@@ -26,33 +26,34 @@ async function main() {
 	const speaker = new Speaker({ sampleRate: 48000, channels: 1, bitDepth: 16 })
 	speaker.cork()
 	setTimeout(() => speaker.uncork(), 1000)
-	const audioBuffer = new Int16Array(48000 * .1)
-	const audioData = new Uint8Array(audioBuffer.buffer, audioBuffer.byteOffset, audioBuffer.byteLength)
+	let audioArray = new Int16Array(48000 * .1)
 	let bufferFill = 0
 	const audioSignal = (x: number) => {
 		x *= .7
-		audioBuffer[bufferFill++] = Math.round(x * (2**15 - 1))
-		if (bufferFill >= audioBuffer.length) {
-			speaker.write(audioData.slice())
+		audioArray[bufferFill++] = Math.round(x * (2**15 - 1))
+		if (bufferFill >= audioArray.length) {
+			speaker.write(Buffer.from(audioArray.buffer, audioArray.byteOffset, audioArray.byteLength))
+			// we can't touch buffer we've written; allocate a new one
+			audioArray = new Int16Array(audioArray.length)
 			bufferFill = 0
 		}
 	}
 
 	// DSP chain
-	const audioFilter = makeDecimatingFIR(audioSignal, lowPassTaps(fs/5, 16e3, 5e3, 40), 5)
-	const demod = makeFreqDemodulator(fs/5, carrierDeviation)
+	const audioFilter = makeDecimatingFirF(audioSignal, lowPassTaps(fs/5, 16e3, 5e3, 40), 5)
+	const demod = makeFrequencyDemod(fs/5, carrierDeviation)
 	const basebandSignal = (x: Complex) => audioFilter(demod(x))
 
-	const channelFilter = makeCDecimatingFIR(basebandSignal, lowPassTaps(fs, 75e3, 10e3, 40), 5)
+	const channelFilter = makeDecimatingFirC(basebandSignal, lowPassTaps(fs, 75e3, 10e3, 40), 5)
 	const shifter = makeShifter(fs, tuneOffset)
 	const signal = (x: Complex) => channelFilter(shifter(x))
 
 	console.error(`Receiving at ${(carrierFrequency/1e6).toFixed(2)}MHz...`)
 	process.on('SIGINT', () => device.requestStop())
-	await device.receive(buffer => {
-		const samples = buffer.length / 2
+	await device.receive(array => {
+		const samples = array.length / 2
 		for (let n = 0; n < samples; n++)
-			signal([ buffer[n * 2 + 0] / 127, buffer[n * 2 + 1] / 127 ])
+			signal([ array[n * 2 + 0] / 127, array[n * 2 + 1] / 127 ])
 	})
 	speaker.destroy()
 	console.error('\nDone, exiting')
@@ -67,11 +68,11 @@ const multiply = (a: Complex, b: Complex): Complex =>
 	[ a[0]*b[0] - a[1]*b[1], a[1]*b[0] + a[0]*b[1] ]
 
 function makeShifter(fs: number, shiftFrequency: number) {
-	const localOscillator = makeCToneGenerator(fs, shiftFrequency)
+	const localOscillator = makeToneGeneratorC(fs, shiftFrequency)
 	return (x: Complex) => multiply(localOscillator(), x)
 }
 
-function makeCToneGenerator(fs: number, frequency: number, amplitude: number = 1) {
+function makeToneGeneratorC(fs: number, frequency: number, amplitude: number = 1) {
 	let phase = 0
 	const delta = (TAU * frequency / fs) % TAU
 	return (): Complex => {
@@ -80,7 +81,7 @@ function makeCToneGenerator(fs: number, frequency: number, amplitude: number = 1
 	}
 }
 
-function makeFreqDemodulator(fs: number, maxDeviation: number) {
+function makeFrequencyDemod(fs: number, maxDeviation: number) {
 	let phase = 0
 	const iDelta = 1 / (TAU * maxDeviation / fs)
 	return (x: Complex): number => {
@@ -106,7 +107,7 @@ function lowPassTaps(fs: number, cutoffFreq: number, transitionWidth: number, at
 	return taps.map(x => gain * x)
 }
 
-function makeDecimatingFIR(output: (x: number) => void, taps: number[], decimation: number) {
+function makeDecimatingFirF(output: (x: number) => void, taps: number[], decimation: number) {
 	const hs = Float64Array.from(taps).reverse()
 	const xs = new Float64Array(taps.length)
 	let start = 0
@@ -124,7 +125,7 @@ function makeDecimatingFIR(output: (x: number) => void, taps: number[], decimati
 	}
 }
 
-function makeCDecimatingFIR(output: (x: Complex) => void, taps: number[], decimation: number) {
+function makeDecimatingFirC(output: (x: Complex) => void, taps: number[], decimation: number) {
 	const hs = Float64Array.from(taps).reverse()
 	const xs = new Float64Array(2 * taps.length)
 	let start = 0
